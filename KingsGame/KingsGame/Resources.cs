4561af8560205933601d378c891b85693504b9e2
Jean-Christophe Martel <tramkotipo_18@hotmail.com>
2018-05-12 19:44:20 -0400

    Adding resources class

---
+++ KingsGame/KingsGame/Resources.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingsGame {
+    public class Resources {
+        public Type Type { get; set; }
+
+        public Resources(Type type) {
+            Type = type;
+        }
+    }
+
+    public enum Type {
+        Wheat,
+        Rock,
+        Steel,
+        Wood,
+        Clay
+    }
+}
